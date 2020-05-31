b19e8c8db01680c358d9b05134d8e6673c90eb63
EC2 Default User <ec2-user@ip-172-31-58-168.ec2.internal>
2020-05-31 20:38:32 +0000

    pushing local code to git for the first time

---
+++ make_prediction_k8s.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+PORT=8001
+echo "Port: $PORT"
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
+# POST method predict
+curl -d '{  
+   "CHAS":{  
+      "0":0
+   },
+   "RM":{  
+      "0":6.575
+   },
+   "TAX":{  
+      "0":296.0
+   },
+   "PTRATIO":{  
+      "0":15.3
+   },
+   "B":{  
+      "0":396.9
+   },
+   "LSTAT":{  
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://localhost:$PORT/api/v1/namespaces/default/pods/$POD_NAME/proxy/predict
+
+kubectl logs -f $POD_NAME
